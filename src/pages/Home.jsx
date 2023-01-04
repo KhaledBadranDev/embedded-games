5cca3f761a976af96769f0d07f84a4868d6e3581
Khaled Badran <gym4programming@gmail.com>
2023-01-03 22:01:56 -0700

    implementing some helper/utils functions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import {isDocInDb} from "../utils/utils"
+
+const test = async()=>{
+    try {
+        const res = await isDocInDb("scratch", 123)        
+        console.log("res::", res)
+    } catch (error) {
+        console.log("error::", error)
+    }
+}
 
 const Home = () => {
+    test()
+    
+
     return (
         <div className="container text-center text-white mt-5">
             <h1>Home</h1>
