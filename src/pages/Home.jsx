5fefca994e1b5b7e78e8e0d686cfbc42cdb21355
Khaled Badran <gym4programming@gmail.com>
2023-01-05 21:42:47 -0700

    remove tmp testing lines of code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,6 @@
 import React from "react";
-import {isDocInDb} from "../utils/utils"
-
-const test = async()=>{
-    try {
-        const res = await isDocInDb("scratch", 123)        
-        console.log("res::", res)
-    } catch (error) {
-        console.log("error::", error)
-    }
-}
 
 const Home = () => {
-    test()
-    
 
     return (
         <div className="container text-center text-white mt-5">
