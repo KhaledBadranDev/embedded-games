2587cefc4044593da48690b92b4ef4f437edada9
Khaled Badran <gym4programming@gmail.com>
2023-01-06 18:25:51 -0700

    WIP basic UI for updating a doc

---
+++ src/components/UpdateGame.jsx
@@ -0,0 +1,50 @@
+import { React, useState, useCallback } from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { handleUpdateSubmission } from "../utils/handleSubmissions"
+
+const UpdateGame = ({ platform, admin }) => {
+    const [id, setId] = useState()
+
+    // using arrow functions or binding in JSX is a bad practice as it hurts the performance.
+    // because the function is recreated on each render.
+    // to solve this issue, use the callback with the useCallback() hook,
+    // and assign the dependencies to an empty array.
+    const setIdCallBack = useCallback(event => setId(event.target.value), [])
+    const handleUpdateSubmissionCallBack = useCallback(event => handleUpdateSubmission(event, id, platform, admin), [id, platform, admin])
+
+    return (
+        <div className="mt-5 container bg-dark rounded rounded-3 shadow-lg pt-3 pb-5">
+            <h4 className="text-white text-center">Update a {platform} Game</h4>
+            <div className="container text-white">
+                <Form onSubmit={handleUpdateSubmissionCallBack}>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+
+                        <Form.Label>Game Id</Form.Label>
+                        <Form.Control
+                            type="Game ID"
+                            placeholder="Enter id e.g. 723650095"
+                            required
+                            value={id}
+                            onChange={setIdCallBack}
+                        />
+
+                        <Form.Text className="text-muted mt-2">
+                            All the other details will be fetched automatically using Scratch REST API.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <div className="d-grid gap-2 col-6 mx-auto">
+                        <Button variant="success" type="submit">
+                            Submit
+                        </Button>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default UpdateGame
