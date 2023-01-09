51698f59a8d4f68ae21f1fc59e6fe118408508cd
Khaled Badran <gym4programming@gmail.com>
2023-01-09 13:14:52 -0700

    test adding .gif files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import gif1 from "../assets/gif1.gif";
 
 const Home = () => {
 
     return (
-        <div className="container text-center text-white mt-5">
+        <div className="container text-center text-white mt-5 ">
             <h1>Home</h1>
+            <img src={gif1} alt="gif" style={{width:"30%",height:"30%"}}/>
         </div>
     );
 };
