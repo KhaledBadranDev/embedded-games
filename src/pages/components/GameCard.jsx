a470c71afae99a23e15c2ef9c2577e92995fefdb
Khaled Badran <gym4programming@gmail.com>
2023-01-10 23:32:24 -0700

    WIP Select and Play a game

--- src/pages/components/GameCard.jsx
+++ src/pages/components/GameCard.jsx
@@ -1,16 +1,28 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { useCallback } from "react";
 
 
-export default function GameCard({gameObj}) {
+export default function GameCard({gameObj, setSelectedGame}) {
+    // using arrow functions or binding in JSX is a bad practice as it hurts the performance.
+    // because the function is recreated on each render.
+    // to solve this issue, use the callback with the useCallback() hook,
+    // and assign the dependencies to an empty array.
+    const handelPlayGame = useCallback(event => {
+        event.preventDefault()
+        console.log(gameObj)
+        setSelectedGame(gameObj)
+    }, [gameObj, setSelectedGame])
+
     return (
         <Card className="bg-dark">
             <Card.Img variant="top" src={gameObj["image"]} />
             <Card.Body className="text-white">
-                <Card.Title className="text-center">{gameObj["title"]}</Card.Title>
-                <Card.Text>{gameObj["description"]}</Card.Text>
+                <Card.Title className="text-center h6">{gameObj["title"]}</Card.Title>
+                <Card.Text className="text-muted mb-0">author: {gameObj["author"]["username"]}</Card.Text>
+                <Card.Text className="text-muted">admin: {gameObj["adminEmail"]}</Card.Text>
                 <div className="d-grid gap-2">
-                    <Button variant="primary">Play</Button>
+                    <Button variant="primary" onClick={handelPlayGame}>Play</Button>
                 </div>
             </Card.Body>
         </Card>
--- src/pages/components/StructureGamesCards.jsx
+++ src/pages/components/StructureGamesCards.jsx
@@ -1,17 +1,28 @@
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import GameCard from './GameCard';
+// import SelectedGameModal from './SelectedGameModal';
+import { useState } from "react";
 
 const StructureGamesCards = (gamesArr, platform) => {
+    const [selectedGame, setSelectedGame] = useState({})
+
+
     return (
         <article className="container">
             <Row xs={1} md={2} lg={3} className="g-4">
                 {Array.from({ length: gamesArr.length }).map((_, i) => (
                     <Col key={gamesArr[i]["id"]} >
-                        <GameCard gameObj={gamesArr[i]}></GameCard>
+                        <GameCard gameObj={gamesArr[i]} setSelectedGame={setSelectedGame} ></GameCard>
                     </Col>
                 ))} 
             </Row>
+            {/* selectedGame 👈 to check if the obj null or undefined */}
+            { (selectedGame && Object.keys(selectedGame).length !== 0) && 
+                <>
+                    {console.log("selectedGame", selectedGame)}
+                </>
+            }
         </article>
     );
 }
