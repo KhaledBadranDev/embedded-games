2c5ba1c1d0e8367051eaf2491c8da31f688217b2
Khaled Badran <gym4programming@gmail.com>
2023-01-12 14:48:40 -0700

    fix toggle icon

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import CustomLink from "./CustomLink";
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 import './Navbar.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+// reference/documentation: https://dev.to/johnotu/how-to-toggle-bootstrap-navbar-collapse-button-in-react-without-jquery-joo
 
 const Navbar = () => {
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const handleNavCollapseCallBack = useCallback(() => setIsNavCollapsed(!isNavCollapsed), [isNavCollapsed]);
+
     return (
-        <nav className="navbar navbar-expand-sm bg-dark navbar-dark py-2">
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark py-2">
             <div className="container">
-                <Link to="./" className="nav-link text-white">EmbeddedGames</Link>
-                <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav ms-auto">
-                        <li className="nav-item ">
+                <Link to="./" className="navbar-brand text-info font-weight-bolder">EmbeddedGames</Link>
+
+                <button class="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#toggleableNavbar" aria-controls="toggleableNavbar" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapseCallBack}>
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="toggleableNavbar">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
                             <CustomLink to="./" className="nav-link text-primary">Home</CustomLink>
                         </li>
                         <li className="nav-item">
@@ -26,7 +38,8 @@ const Navbar = () => {
                 </div>
             </div>
         </nav>
-    )
+    );
+
 }
 
 export default Navbar
